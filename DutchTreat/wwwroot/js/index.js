176a6ced5e885d424f42293a7820cb3d08b0b568
Josiah G. Smythe <jsmythe@DEVBASELINE>
2020-08-14 16:25:49 -0400

    Continued Training and begun adding js

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,14 @@
+﻿var x = 0;
+
+
+console.log("Hello World");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("productProps");
