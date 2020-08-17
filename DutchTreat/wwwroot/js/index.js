a4be8ac8526bd6b8a0b8687186ebc774e09bb0a4
Josiah G. Smythe <jsmythe@DEVBASELINE>
2020-08-17 11:18:34 -0400

    Continued adding jquery and html

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,14 +1,29 @@
-﻿var x = 0;
+﻿$(document).ready(function () {
+    var x = 0;
 
 
-console.log("Hello World");
+    console.log("Hello World");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".productProps li");
+    productInfo.on("click", function () {
+        consol.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle();
+    });
+
 });
-
-var productInfo = document.getElementsByClassName("productProps");
